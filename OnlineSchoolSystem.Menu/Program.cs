0f3dfac1c481d524883227c4fbc3eef42b7acf7d
emedvedeva <35913899+emedvedeva@users.noreply.github.com>
2021-04-03 19:03:42 +0300

    Created project Menu

---
+++ OnlineSchoolSystem.Menu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnlineSchoolSystem.Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
