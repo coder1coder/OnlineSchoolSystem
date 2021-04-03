17c5a5ae7c8945d2600195bb2bbc1852deb08107
Андрей Федерягин <eniluck@mail.ru>
2021-04-03 21:38:47 +0500

    feat: new message structure used in json file methods

--- OnlineSchoolSystem.DataAccess.File/JsonFileAccess.cs
+++ OnlineSchoolSystem.DataAccess.File/JsonFileAccess.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using System.Linq;
+using OnlineSchoolSystem.Models;
 
 namespace OnlineSchoolSystem.DataAccess.FileStorage
 {
-    //https://docs.microsoft.com/ru-ru/dotnet/standard/serialization/system-text-json-how-to?pivots=dotnet-core-3-1
     public class JsonFileAccess
     {
         private readonly string fileName;
@@ -16,7 +16,7 @@ namespace OnlineSchoolSystem.DataAccess.FileStorage
         }
 
         // записываем сообщения в файлы
-        private void WriteMessagesToFile(List<StubChatMessageEntity> chatMessages)
+        private void WriteMessagesToFile(List<Message> chatMessages)
         {
             string jsonString = JsonConvert.SerializeObject(chatMessages, Formatting.Indented);
 
@@ -24,7 +24,7 @@ namespace OnlineSchoolSystem.DataAccess.FileStorage
         }
 
         // Записать в файл сообщение
-        public void AppendMessageToFile(StubChatMessageEntity chatMessage)
+        public void AppendMessageToFile(Message chatMessage)
         {
             var storedChatMessages = ReadAllMessagesFromFile();
             storedChatMessages.Add(chatMessage);
@@ -34,7 +34,7 @@ namespace OnlineSchoolSystem.DataAccess.FileStorage
         }
 
         // Записать в файл сообщения
-        public void AppendMessagesToFile(List<StubChatMessageEntity> chatMessages)
+        public void AppendMessagesToFile(List<Message> chatMessages)
         {
             var storedChatMessages = ReadAllMessagesFromFile();
             storedChatMessages.AddRange(chatMessages);
@@ -44,37 +44,37 @@ namespace OnlineSchoolSystem.DataAccess.FileStorage
         }
 
         // Удалить сообщение
-        public void DeleteMessage(StubChatMessageEntity chatMessages)
+        public void DeleteMessage(Message chatMessages)
         {
             // найти и удалить по совпадению чего?
             throw new NotImplementedException();
         }
 
         // Удалить сообщения
-        public void DeleteMessages(List<StubChatMessageEntity> chatMessages)
+        public void DeleteMessages(List<Message> chatMessages)
         {
             // найти и удалить по совпадению чего?
             throw new NotImplementedException();
         }
 
         // Прочитать из файла
-        public List<StubChatMessageEntity> ReadAllMessagesFromFile()
+        public List<Message> ReadAllMessagesFromFile()
         {
             if (File.Exists(fileName))
             {
                 string json = File.ReadAllText(fileName);
-                var chatMessages = JsonConvert.DeserializeObject<List<StubChatMessageEntity>>(json);
+                var chatMessages = JsonConvert.DeserializeObject<List<Message>>(json);
                 if (chatMessages == null)
-                    return new List<StubChatMessageEntity>();
+                    return new List<Message>();
                 return chatMessages;
             }
             else
             {
-                return new List<StubChatMessageEntity>();
+                return new List<Message>();
             }
         }
 
-        public List<StubChatMessageEntity> GetUniqueValues(List<StubChatMessageEntity> chatMessages)
+        public List<Message> GetUniqueValues(List<Message> chatMessages)
         {
             //для сравнения по определённым полям реализовать IEquatable<StubChatMessageEntity> для StubChatMessageEntity
             //https://docs.microsoft.com/ru-ru/dotnet/api/system.linq.enumerable.distinct?view=net-5.0
