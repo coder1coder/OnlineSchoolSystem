4c869d5ae7c2fc3b37184894202a27b29df6d76e
Михаил Кодер <coder1coder@gmail.com>
2021-04-03 22:19:55 +0300

    Move BroadCast class from YoutubeBot to Models

---
+++ OnlineSchoolSystem.Models/Broadcast.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineSchoolSystem.Models
+{
+    public class Broadcast
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string LiveChatId { get; set; }
+    }
+}
