4bfecd7817969fc4bd891ee875d655cc127e95b7
Михаил Кодер <coder1coder@gmail.com>
2021-04-04 14:44:42 +0300

    Удалил ненужную реализацию в Program проекта Menu

--- OnlineSchoolSystem.Menu/Program.cs
+++ OnlineSchoolSystem.Menu/Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
-
-namespace OnlineSchoolSystem.Menu
+﻿namespace OnlineSchoolSystem.Menu
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MenuController menu = new OnlineSchoolSystem.Menu();
-            menu.Start();
+
         }
     }
 }
